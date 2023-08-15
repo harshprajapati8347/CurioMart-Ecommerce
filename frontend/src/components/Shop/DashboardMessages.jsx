7672763b7fcaaf373c03b86ac4c57a3369a6aec7
harshprajapati8347 <harshprajapati8347@gmail.com>
2023-08-15 15:10:17 +0530

    feat-socket url env deploy ready

--- frontend/src/components/Shop/DashboardMessages.jsx
+++ frontend/src/components/Shop/DashboardMessages.jsx
@@ -9,7 +9,7 @@ import styles from "../../styles/styles";
 import { TfiGallery } from "react-icons/tfi";
 import socketIO from "socket.io-client";
 import { format } from "timeago.js";
-const ENDPOINT = "https://curiomart-socket-harsh.onrender.com/";
+const ENDPOINT = process.env.SOCKET_URL;
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const DashboardMessages = () => {
--- frontend/src/pages/UserInbox.jsx
+++ frontend/src/pages/UserInbox.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowRight, AiOutlineSend } from "react-icons/ai";
 import { TfiGallery } from "react-icons/tfi";
 import styles from "../styles/styles";
-const ENDPOINT = "https://curiomart-socket-harsh.onrender.com/";
+const ENDPOINT = process.env.SOCKET_URL;
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const UserInbox = () => {
