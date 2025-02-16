5b22eca47462aa83539ae75b4e4cd7d4ed58b78a
Harsh Prajapati <harshprajapati8347@gmail.com>
2025-02-16 16:04:59 +0530

    feat - add subdomain to cors

--- socket/index.js
+++ socket/index.js
@@ -17,6 +17,7 @@ app.use(
       "http://localhost:3000",
       "http://127.0.0.1:3000",
       "https://curiomart.vercel.app",
+      "https://curiomart.iamharsh.in",
     ],
     credentials: true,
   })
